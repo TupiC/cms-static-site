e6afea8703c3b92883f4da6d6f1965f524d82a41
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2022-10-13 22:00:36 +0200

    small changes

--- src/js/lib/mainnav.js
+++ src/js/lib/mainnav.js
@@ -1,12 +1,15 @@
 export function init() {
     const hamburger = document.querySelector("#hamburger");
+    const hamburgerContainer = document.querySelector(".menu");
     hamburger.addEventListener("click", () => {
         if (hamburger.classList.contains("closed")) {
             hamburger.classList.add("open");
             hamburger.classList.remove("closed");
+            hamburgerContainer.classList.add('hamburgerContainer');
         } else {
             hamburger.classList.add("closed");
             hamburger.classList.remove("open");
+            hamburgerContainer.classList.remove('hamburgerContainer');
 
         }
     })
