293108ba5363cc597b85e993d31367c3ca06c19e
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2022-10-13 21:08:45 +0200

    Finished side style + burger animation

--- src/js/lib/mainnav.js
+++ src/js/lib/mainnav.js
@@ -1,5 +1,13 @@
 export function init() {
-    document.querySelector("#hamburger.menu").addEventListener("click", () => {
-        document.querySelector("#hamburger.menuItems")
+    const hamburger = document.querySelector("#hamburger");
+    hamburger.addEventListener("click", () => {
+        if (hamburger.classList.contains("closed")) {
+            hamburger.classList.add("open");
+            hamburger.classList.remove("closed");
+        } else {
+            hamburger.classList.add("closed");
+            hamburger.classList.remove("open");
+
+        }
     })
 }
